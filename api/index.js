0e021fc87d80141367d6bbeb37bc6e4375d95e4d
Jitesh52142 <jitesh52142@gmail.com>
2025-10-17 18:11:08 +0530

    Fix path-to-regexp error: Remove problematic wildcard route

--- api/index.js
+++ api/index.js
@@ -280,8 +280,12 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
-// Catch-all handler
-app.get('*', (req, res) => {
+// Handle other routes
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
